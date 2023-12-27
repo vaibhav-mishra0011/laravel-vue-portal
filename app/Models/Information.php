0360533b0232925afa64a2e13c686cdd6650579e
vaibhav-mishra0011 <vaibhav.mishra@infoxen.com>
2023-12-27 18:29:35 +0530

    make layout

--- app/Models/Information.php
+++ app/Models/Information.php
@@ -5,12 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ExcelData extends Model
+class Information extends Model
 {
     use HasFactory;
-    protected $fillable = [
-        'first_name',
-        'last_name',
-        'email',
-    ];
 }
